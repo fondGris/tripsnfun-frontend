62b46eeb2bd7b1508f252deaecabd00107baa034
JulienQ <jquan84@gmail.com>
2022-12-12 15:41:29 +0100

    un peu de color et de style pour le login screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,6 +2,7 @@ import { Button, StyleSheet, Text, View, Image } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import { useEffect, useState } from 'react';
 import * as Location from 'expo-location';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 export default function HomeScreen() {
 
@@ -22,28 +23,38 @@ export default function HomeScreen() {
     }, []);
     // console.log(currentPosition)
     // to make the map set on the user position
-let initialPosition = null
-if (currentPosition == null ) 
-{return} else
-{
-    
-    initialPosition = {
-        latitude: currentPosition.latitude,
-        longitude: currentPosition.longitude,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
+    let initialPosition = null
+    if (currentPosition == null) { return } else {
+        initialPosition = {
+            name: 'Ma Position',
+            latitude: currentPosition.latitude,
+            longitude: currentPosition.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
 
+        }
     }
-} 
 
+    const otherUsersData = [
+        { name: 'Kassim', latitude: 48.859, longitude: 2.347 },
+        { name: 'Farouk', latitude: 48.29, longitude: 4.074 },
+        { name: 'Yssam', latitude: 43.282, longitude: 5.405 },
+        { name: 'Marie', latitude: 43.091 , longitude:-0.045 },
+
+    ]
+
+    const otherUsers = otherUsersData.map(( data, i) => {
+        return <Marker key={i} coordinate={{ latitude: data.latitude, longitude: data.longitude }} title={data.name} pinColor="#fecb2d" />;
+    })
+    
 
     return (
         <View style={styles.container}>
             <Text> Home Screen</Text>
             <MapView style={styles.map} initialRegion={initialPosition} showsUserLocation followsUserLocation >
 
-                {currentPosition && <Marker coordinate={currentPosition} title="My position" pinColor="#fecb2d" />}
-
+                {currentPosition && <Marker coordinate={currentPosition} title="My position"   />}
+                {otherUsers}
             </MapView>
         </View>
 
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -6,7 +6,7 @@ export default function LoginScreen({ navigation }) {
 
             <Text style={styles.title}>Login Screen</Text>
 
-            <TextInput style={styles.input} placeholder="Login" />
+            <TextInput style={styles.input} placeholder="Email" />
             <TextInput style={styles.input} placeholder="Password" />
             <TouchableOpacity onPress={() => navigation.navigate('TabNavigator')} style={styles.button} activeOpacity={0.8}>
                 <Text style={styles.textButton}>Submit</Text>
@@ -19,15 +19,33 @@ const styles = StyleSheet.create({
 
     container: {
         flex: 1,
-        backgroundColor: 'red',
-    },
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+
+    title: {
+        width: '80%',
+        fontSize: 38,
+        fontWeight: '600',
+      },
+
     input: {
         width: '80%',
         marginTop: 25,
-        borderBottomColor: '#ec6e5b',
+        borderBottomColor: 'orange',
         borderBottomWidth: 1,
         fontSize: 18,
     },
+    button: {
+        alignItems: 'center',
+        paddingTop: 8,
+        width: '80%',
+        marginTop: 30,
+        backgroundColor: 'green',
+        borderRadius: 10,
+        marginBottom: 80,
+      },
     textButton: {
         color: '#ffffff',
         height: 30,
