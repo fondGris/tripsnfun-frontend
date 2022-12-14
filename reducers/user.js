6fe6a4c6f27067a06130947953ef99197fead192
Kassim <kassimbouzoubaa05@gmail.com>
2022-12-14 13:07:12 +0100

    Markers autour de moi

--- reducers/user.js
+++ reducers/user.js
@@ -1,23 +1,27 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  value: { token: null, username: null },
+  value: { token: null, username: null, markers: [] },
 };
 
 export const userSlice = createSlice({
-  name: 'user',
+  name: "user",
   initialState,
   reducers: {
     login: (state, action) => {
-      state.value.token = action.payload.token;
+      state.value.token = action.payload.token; // NE PAS OUBLIER DE MODIFIER PUSH PAR =
       state.value.username = action.payload.username;
     },
     logout: (state) => {
       state.value.token = null;
       state.value.username = null;
     },
+    addAllMarkers: (state, action) => {
+      state.value.markers = action.payload;
+    },
+    
   },
 });
 
-export const { login, logout } = userSlice.actions;
+export const { login, logout, addAllMarkers } = userSlice.actions;
 export default userSlice.reducer;
