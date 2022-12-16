dcb1836ea7959de1dc1992f3db3fc1cad6a567fd
Kassim <kassimbouzoubaa05@gmail.com>
2022-12-16 12:29:20 +0100

    modification du marker dans mapScreen

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -6,7 +6,7 @@ import { login, logout, removeAllMarkers } from '../reducers/user';
 export default function ProfileScreen({ navigation }) {
 	const dispatch = useDispatch();
     const user = useSelector((state) => state.user.value);
-    const BACKEND_ADDRESS = "http://172.20.10.12:3000";
+    const BACKEND_ADDRESS = "http://192.168.10.105:3000";
 
 
 	const handleLogout = () => {
